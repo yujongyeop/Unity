d1979e0e68b562b73bcc2cdebc824ff7bba54fd4
Platform1117 <230233@naver.com>
2022-10-04 14:10:08 +0900

    21

--- Assets/ObjectHit.cs
+++ Assets/ObjectHit.cs
@@ -7,5 +7,7 @@ public class ObjectHit : MonoBehaviour
     //물체가 무언가에 부딪혔을 때 실행
     private void OnCollisionEnter(Collision other) {
         Debug.Log("Bumped into a wall");
+        
+        GetComponent<MeshRenderer>().material.color = Color.black;
     }
 }
