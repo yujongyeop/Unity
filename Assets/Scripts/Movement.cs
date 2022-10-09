5af5ed5486037f5955a2b8bc2e0a88f1e2a87807
Platform1117 <230233@naver.com>
2022-10-09 17:51:54 +0900

    ch.40

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody rb;
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rotationThrust = 2f;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
 
     }
 
@@ -21,18 +25,32 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("Pressed Space - Thrusting");
+            // Debug.Log("Pressed Space - Thrusting");
+            // Vector3.up = (0, 1, 0)
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
         }
     }
     void ProcessRotation()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("Rotating Left");
+            // Debug.Log("Rotating Left");
+
+            // Vector3.forward = (0, 0, 1)
+            ApplyRotation(rotationThrust);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("Rotating Right");
+            // Debug.Log("Rotating Right");
+
+            ApplyRotation(-rotationThrust);
         }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; // 수동제어를 할 수 있도록 회전을 고정한다.
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; // 물리 시스템이 적용되도록 회전 고정을 해제한다.
     }
 }
