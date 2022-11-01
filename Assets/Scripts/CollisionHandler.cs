25a2a45a57380f956bc4c776035f354758aafcbb
Platform1117 <230233@naver.com>
2022-11-02 01:30:31 +0900

    Solve Collision Error
    중복 충돌에 관한 처리 추가

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-    [SerializeField] float levelLoadDelay = 1f;
+    [SerializeField] float levelLoadDelay = 2f;
     [SerializeField] AudioClip success;
     [SerializeField] AudioClip crash;
+
     AudioSource audioSource;
 
-    void Start(){
+    //충돌 여부(중복 충돌 방지를 위함)
+    bool isTransitioning = false;
+
+    void Start()
+    {
         audioSource = GetComponent<AudioSource>();
     }
 
     void OnCollisionEnter(Collision other)
     {
+        if (isTransitioning) { return; }
         switch (other.gameObject.tag)
         {
             case "Friendly":
@@ -33,6 +39,10 @@ public class CollisionHandler : MonoBehaviour
     //충돌 시 처리
     void StartCrashSequence()
     {
+        //충돌 여부를 True 설정
+        isTransitioning = true;
+        //기존에 재생중이던 소리 멈춤
+        audioSource.Stop();
         //실패 효과음 재생
         audioSource.PlayOneShot(crash);
         // Movement 스크립트 비활성화(로켓 제어권 회수)
@@ -42,7 +52,12 @@ public class CollisionHandler : MonoBehaviour
     }
 
     //스테이지 클리어 시 처리
-    void StartSuccessSequence(){
+    void StartSuccessSequence()
+    {
+        //충돌 여부를 True 설정
+        isTransitioning = true;
+        //기존에 재생중이던 소리 멈춤
+        audioSource.Stop();
         //성공 효과음 재생
         audioSource.PlayOneShot(success);
         // Movement 스크립트 비활성화(로켓 제어권 회수)
@@ -72,3 +87,4 @@ public class CollisionHandler : MonoBehaviour
         SceneManager.LoadScene(nextSceneIndex);
     }
 }
+
