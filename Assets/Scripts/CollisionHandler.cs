975b704ce9ecc35907659c19976ea0dc4108663e
Platform1117 <230233@naver.com>
2022-11-02 00:00:10 +0900

    Add Delay
    씬 변환 및 게임 재시작 시 Invoke를 통해 딜레이 설정

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -5,35 +5,52 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnCollisionEnter(Collision other) {
-        switch(other.gameObject.tag){
+    [SerializeField] float levelLoadDelay = 1f;
+    void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
             case "Friendly":
                 Debug.Log("This thing is friendly");
                 break;
             case "Finish":
-                LoadNextLevel();
-                break;
-            case "Fuel":
-                Debug.Log("You picked up fuel");
+                StartSuccessSequence();
                 break;
             default:
-                ReloadLevel();
+                StartCrashSequence();
                 break;
         }
     }
 
-    void ReloadLevel(){
+    void StartCrashSequence()
+    {
+        // Movement 스크립트 비활성화(로켓 제어권 회수)
+        GetComponent<Movement>().enabled = false;
+        // 1초 지연 후 ReloadLevel 호출
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    void StartSuccessSequence(){
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+
+    void ReloadLevel()
+    {
         // 현재 씬의 인덱스 번호를 가져옴
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; 
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        // 현재 씬을 
         SceneManager.LoadScene(currentSceneIndex);
     }
 
-    void LoadNextLevel(){
-        
+    void LoadNextLevel()
+    {
+
         // 현재 씬의 인덱스 번호를 가져옴
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; 
-        int nextSceneIndex = currentSceneIndex+1;
-        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
             nextSceneIndex = 0;
         SceneManager.LoadScene(nextSceneIndex);
     }
